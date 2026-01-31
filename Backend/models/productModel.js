c013d32c35050b8dd804477dc942ea9f897390c0
Sasidharan V <102961800+sasi72@users.noreply.github.com>
2026-01-31 20:55:03 +0530

    Updated product schema details

--- Backend/models/productModel.js
+++ Backend/models/productModel.js
@@ -2,9 +2,85 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.schema({
   name: {
-    type: string,
+    type: String,
     required: [true, "Please enter product name"],
     trim: true,
     maxLength: [100, "Product name cannot exceed 100 characters"]
+  },
+  price: {
+    type: Number,
+    default: 0.0
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter product description"]
+  },
+  ratings: {
+    type: String,
+    default: 0
+  },
+  images: [{
+    image: {
+      type: String,
+      required: [true,"Please provide product images"]
+    }
+  }],
+  category: {
+    type: String,
+    required: [true, "Please provide product category"],
+    enum: {
+      values: [
+        'Electronics',
+        'MobilrPhone',
+        'Laptops',
+        'Accessories',
+        'Headphones',
+        'Food',
+        'Books',
+        'Clothes/Shoes',
+        'Beauty/Health',
+        'Sports',
+        'Outdoor',
+        'Home'
+      ],
+      message: "Please select only allowed categories"
+    }
+  },
+  seller: {
+    type: String,
+    requred: [true, "Please enter product seller"]
+  },
+  stock: {
+    type: Number,
+    required: [true,"Please enter product stocks"],
+    maxLengh: [20,"Product stocks are limited to 20"]
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true
+      },
+      rating: {
+        type: String,
+        required: true
+      },
+      comment: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now()
   }
 })
+
+let schema = mongoose.model('Product',productSchema)
+
+module.exports = schema
